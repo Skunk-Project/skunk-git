427232881471f7e5d437740a3ab1f5dbb7b9ce31
Zürcher Marco (IT-SCI-DSI-KI) <marco.zuercher@sbb.ch>
2016-06-17 15:09:30 +0200

    und Programm

---
+++ PHP_Kurs/Kursdateien/und.php
@@ -0,0 +1,12 @@
+<html>
+<body>
+<?php
+    if ($_POST["bname"] == "Maier" && $_POST["pw"] == "kuckuck")
+        echo "Zugang gestattet";
+    else
+        echo "Zugang verweigert";
+?>
+</body>
+</html>
+
+
